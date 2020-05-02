27decb1fdc3e33202395015f7aceb0f92431bcc1
ariarrazak10 <ariarrazak25@gmail.com>
2020-05-02 23:00:36 +0900

    add some routes

--- app.js
+++ app.js
@@ -17,72 +17,103 @@ app.get('/puskesmas', (req, res) => {
 })
 
 app.post('/puskesmas', (req, res) => {
-    if (req.body.name.length === 0) {
-      return res.status(400).send({
-        success: false,
-        message: 'Puskesmas must have a name'
-      })
-    }
-  
-    puskesmasdb.push({
-      id: db[db.length-1].id + 1,
-      name: req.body.name,
-      description: req.body.description
+  if (req.body.name.length === 0) {
+    return res.status(400).send({
+      success: false,
+      message: 'Puskesmas must have a name'
     })
-  
-    return res.status(201).send({
-      success: true,
-      message: 'New Puskesmas successfully created'
-    })
+  }
+
+  puskesmasdb.push({
+    id: db[db.length - 1].id + 1,
+    name: req.body.name,
+    description: req.body.description
   })
-  app.get('/puskesmas/:id', (req, res) => {
-    let puskesmas = puskesmasdb.filter(item => item.id == req.params.id)
-    if (puskesmas.length === 0) {
-      return res.status(404).send({
-        success: false,
-        message: 'Puskesmas not found'
-      })
-    }
-    return res.status(200).send({
-      success: true,
-      message: `Here is the puskesmas of id ${req.params.id}`,
-      puskesmas: db.puskesmas[0]
-    })
+
+  return res.status(201).send({
+    success: true,
+    message: 'New Puskesmas successfully created'
   })
-  
-  app.put('/puskesmas/:id', (req, res) => {
-    let puskesmas = puskesmasdb.filter(item => item.id == req.params.id)
-    if (puskesmas.length === 0) {
-      return res.status(404).send({
-        success: false,
-        message: 'Puskesmas not found'
-      })
-    }
-  
-    puskesmasdb = puskesmasdb.map(item => {
-      if (item.id == req.params.id) item = { id: item.id, ...req.body }
-      return item
+})
+app.get('/puskesmas/:id', (req, res) => {
+  let puskesmas = puskesmasdb.filter(item => item.id == req.params.id)
+  if (puskesmas.length === 0) {
+    return res.status(404).send({
+      success: false,
+      message: 'Puskesmas not found'
     })
-  
-    return res.status(200).send({
-      success: true,
-      message: 'Puskesmas successfully updated',
-    })
+  }
+  return res.status(200).send({
+    success: true,
+    message: `Here is the puskesmas of id ${req.params.id}`,
+    puskesmas: db.puskesmas[0]
   })
-  
-  app.delete('/puskesmas/:id', (req, res) => {
-    let puskesmas = puskesmasdb.filter(item => item.id == req.params.id)
-    if (puskesmas.length === 0) {
-      return res.status(404).send({
-        success: false,
-        message: 'Puskesmas not found'
-      })
-    }
-  
-    puskesmasdb = puskesmasdb.filter(item => item.id != req.params.id)
-  
-    return res.status(200).send({
-      success: true,
-      message: 'puskesmas successfully deleted'
+})
+
+app.put('/puskesmas/:id', (req, res) => {
+  let puskesmas = puskesmasdb.filter(item => item.id == req.params.id)
+  if (puskesmas.length === 0) {
+    return res.status(404).send({
+      success: false,
+      message: 'Puskesmas not found'
     })
+  }
+
+  puskesmasdb = puskesmasdb.map(item => {
+    if (item.id == req.params.id) item = {
+      id: item.id,
+      ...req.body
+    }
+    return item
   })
+
+  return res.status(200).send({
+    success: true,
+    message: 'Puskesmas successfully updated',
+  })
+})
+
+app.delete('/puskesmas/:id', (req, res) => {
+  let puskesmas = puskesmasdb.filter(item => item.id == req.params.id)
+  if (puskesmas.length === 0) {
+    return res.status(404).send({
+      success: false,
+      message: 'Puskesmas not found'
+    })
+  }
+
+  puskesmasdb = puskesmasdb.filter(item => item.id != req.params.id)
+
+  return res.status(200).send({
+    success: true,
+    message: 'puskesmas successfully deleted'
+  })
+})
+
+app.get('/patient', (req, res) => {
+  res.status(200).send({
+    success: true,
+    message: 'Get all patient',
+    patients: patientdb
+  })
+})
+
+app.post('/patient', (req, res) => {
+  if (req.body.name.length === 0) {
+    return res.status(400).send({
+      success: false,
+      message: 'Patient must have a name'
+    })
+  }
+
+  patientdb.push({
+    id: puskesmasdb[puskesmasdb.length - 1].id + 1,
+    name: req.body.name,
+    description: req.body.description
+  })
+
+  return res.status(201).send({
+    success: true,
+    message: 'New Patient successfully created'
+  })
+})
