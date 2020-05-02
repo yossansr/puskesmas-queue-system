96c473302efa8b1467b5db23eff9922fce0f52e6
Yossan Rahmadi <yossansr06@gmail.com>
2020-05-02 22:18:29 +0800

    Add db

---
+++ db/db.js
@@ -0,0 +1,20 @@
+const puskesmas = [
+    {
+        id: 1,
+        name: 'Puskesmas Pelita',
+        description: 'A Puskesmas'
+    }
+]
+
+const patient = [
+    {
+        id: 1,
+        name: 'Yakuza',
+        age: '50'
+    }
+]
+  
+module.exports = {
+    puskesmas,
+    patients
+}
